4cfe1ae1a857b3b56acdd0d9223914ef7ae9a16c
amr <amramrnour2255@gmail.com>
2023-12-21 22:58:09 +0200

    finished splash .

---
+++ lib/ui/screens/splash/splash.dart
@@ -0,0 +1,26 @@
+import 'package:chat_app/ui/screens/auth/login/login.dart';
+import 'package:chat_app/ui/utils/app_assets.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  static const routeName = "splash";
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2),(){
+      Navigator.pushNamed(context, Login.routeName);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(AppAssets.splashScreen,fit: BoxFit.cover,),
+    );
+  }
+}
